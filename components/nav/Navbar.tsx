4c564f076e43b064767acb018f882310563859ec
barangana <baranganadriel@gmail.com>
2024-12-11 23:45:55 -0500

    feat: attempting to fix layouts

--- components/nav/Navbar.tsx
+++ components/nav/Navbar.tsx
@@ -1,17 +1,20 @@
 'use client'
 
-import { signout } from '@/app/(auth)/login/LoginActions'
 import React from 'react'
+import Navlinks from './Navlinks'
+import Link from 'next/link'
+import { signout } from '@/app/(auth)/login/LoginActions'
 
 const Navbar = () => {
   return (
-    <nav className='flex'>
-      <div>Drift</div>
-      <div className='flex items-center justify-start gap-2 grow ml-20'>
-        <button>Home</button>
-        <button>Categories</button>
-        <button>Dailies</button>
-        <button onClick={async () => signout}>Signout</button>
+    <nav className='flex mt-4'>
+      <Link href='/' className='font-bold text-3xl'>
+        Drift
+      </Link>
+      <div className='flex items-center justify-start gap-6 pl-20'>
+        <Navlinks href='/categories'>Categories</Navlinks>
+        <Navlinks href='/dailies'>Dailies</Navlinks>
+        <button onClick={async () => signout()}>Signout</button>
       </div>
     </nav>
   )
